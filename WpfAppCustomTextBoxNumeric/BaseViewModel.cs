e3fde7a3513bb59bac1cfa1a6affba0d5e7b179f
Ajay.Bansode <Ajay.Bansode@DESKTOP-B7N6PTK>
2023-07-03 14:28:37 +0530

    Add project files.

---
+++ WpfAppCustomTextBoxNumeric/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfAppCustomTextBoxNumeric
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+     
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfAppCustomTextBoxNumeric/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCustomTextBoxNumeric
+{
+    public class MainWindowViewModel : BaseViewModel
+
+    {
+
+        public Action<string, bool> OnkeyUP { get; set; }
+
+        public string Numeric { get; set; }
+        public MainWindowViewModel()
+        {
+
+            OnkeyUP = OnkeyUPAction;
+
+
+        }
+        private void OnkeyUPAction(string s, bool b)
+        {
+
+            if (b)
+            {
+                if (Numeric != null)
+                {
+                    Numeric += s;
+                }
+                else
+                {
+                    Numeric = s;
+
+                }
+            }
+            else
+            {
+                if (Numeric != null)
+                {
+                    Numeric.Remove(Numeric.Length - 1);
+                }
+            }
+            OnPropertyChanged(nameof(Numeric));
+            //            MessageBox.Show("Valid Data");
+
+        }
+
+    }
+}
